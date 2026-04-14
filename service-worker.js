d38bf04d53efa05a2baa1ee762a6556c0dd16bcf
psimichelalves-ai <psi.michelalves@gmail.com>
2026-04-14 18:26:15 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE = "metacogni";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(["./"]))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
